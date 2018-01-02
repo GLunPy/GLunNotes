17f007d25c4d3700353976aab3f49cb1ee36969f
Eduardo Areco <areco.eduardo@gmail.com>
2018-01-02 13:05:20 -0300

    iniciando controller.js

---
+++ app/controllers/note.controller.js
@@ -0,0 +1,24 @@
+var Note = require('../models/note.models.js');
+
+exports.create = function(req,res){
+    //crea y guarda una nueva nota
+};
+
+exports.findAll = function(req,res){
+    //busca y muestra todas las notas de la base de datos
+};
+
+exports.findOne = function(req, res) {
+    // Find a single note with a noteId
+
+};
+
+exports.update = function(req, res) {
+    // Update a note identified by the noteId in the request
+
+};
+
+exports.delete = function(req, res) {
+    // Delete a note with the specified noteId in the request
+
+};
