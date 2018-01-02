6aa9ba146e20484ba131d081a0129a1294b03b53
Eduardo Areco <areco.eduardo@gmail.com>
2018-01-02 12:04:52 -0300

    configurado mongodb inicial

--- server.js
+++ server.js
@@ -8,6 +8,33 @@ app.use(bodyParser.urlencoded({ extended:true }))
 //pasar pedidos de contenidos en formato json
 app.use(bodyParser.json())
 
+//configurando la base de datos
+var dbConfig = require('./config/database.config.js');
+    mongoose = require('mongoose');
+    mongoose.connect(dbConfig.url);
+
+/*mongoose.connect(dbConfig.url);
+useMongoClient: true
+}
+
+mongoose.connection.on('error', function(){
+    console.log('no se pudo conectar a la base de datos. Cerrando...');
+    process.exit();
+});
+
+mongoose.connection.once('open', function(){
+    console.log('conectado con exito a la base de datos');
+}) */
+
+mongoose.connection.on('error', function() {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+mongoose.connection.once('open', function() {
+    console.log("Successfully connected to the database");
+})
+
 //defino una ruta simple
 
 app.get('/', function(req, res){
